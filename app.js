f21849a5e800193cc0653bb512bcbd312174da6f
chukwudi5 <stanleyonwumelu@gmail.com>
2024-01-15 11:07:58 -0500

    this is the dumi-web files

---
+++ app.js
@@ -0,0 +1,39 @@
+let myName = "John"
+let myAge = 21 // Let allows you to chnage the data unlike const, this data can change throughout your code execution
+let isAmerican = true
+let isEmpty = null
+let isVacant 
+
+myAge = 500
+myAge = false
+
+console.log(myAge)
+console.log(isVacant)
+
+const isFine = 'ugly'
+const empty = null
+const noExist = 'Anything'
+const old = 78 // you cannot change a variable that is const, this data won't change throughout your code execution
+
+console.log(isFine)
+console.log(old)
+
+let myString = 'Hello world, "BYE"' // If you want to use double quotes in your string, start with a single qoutes. And vice versa.
+console.log(myString)
+console.log(myString.length) // this only gives you a data i think
+
+console.log("mom".length) // just a random string
+
+myAge = "mom".length // you can reassigned an already created variable
+console.log(myAge)
+
+
+
+// LESSON FOR PROPMT AND ALERT
+// Prompt is a way to ask the user to enter an info or data
+
+
+
+
+
+
